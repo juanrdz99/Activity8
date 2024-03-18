3e7bf27c68c6fe0d284a6dca61803ea42a082e3b
juanrdz99 <al02911496@tecmilenio.mx>
2024-03-17 22:29:07 -0600

    Initial commit: Added migrations for teams, divisions, and conferences

---
+++ app/Models/Conferences.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Conferences extends Model
+{
+    use HasFactory;
+}
